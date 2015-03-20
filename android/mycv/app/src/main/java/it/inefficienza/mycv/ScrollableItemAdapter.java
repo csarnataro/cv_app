830f295088f3b0ca340f1b147c4c177299e60109
Christian Sarnataro <c.sarnataro@gmail.com>
2015-03-20 17:00:21 +0100

    Added Android source code

---
+++ android/mycv/app/src/main/java/it/inefficienza/mycv/ScrollableItemAdapter.java
@@ -0,0 +1,74 @@
+package it.inefficienza.mycv;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+/**
+ * @author Christian Sarnataro
+ */
+public class ScrollableItemAdapter extends BaseAdapter {
+
+    private Context context;
+    private List<ScrollableItem> items;
+
+    public ScrollableItemAdapter(Context context, List<ScrollableItem> items) {
+        this.context = context;
+        this.items = items;
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Override
+    public ScrollableItem getItem(int i) {
+        return items.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return items.get(i).hashCode();
+    }
+
+    @Override
+    public View getView(final int position, View view, ViewGroup viewGroup) {
+        if (view==null) {
+            view = LayoutInflater.from(context).inflate(R.layout.work_list_row, null);
+        }
+
+        TextView year = (TextView) view.findViewById(R.id.year);
+        TextView company = (TextView) view.findViewById(R.id.company);
+        TextView role = (TextView) view.findViewById(R.id.role);
+
+        final ScrollableItem scrollableItem = items.get(position);
+
+        if (scrollableItem.getYear() == null) {
+            year.setVisibility(View.GONE);
+        } else {
+            year.setVisibility(View.VISIBLE);
+            year.setText(scrollableItem.getYear());
+            // year.setTextColor(yearTextColor);
+        }
+        company.setText(scrollableItem.getCompany());
+        role.setText(scrollableItem.getRole());
+
+        /*
+        year.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, scrollableItem.getYear(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        */
+
+        return view;
+    }
+}
---
+++ android/mycv/app/src/main/java/it/inefficienza/mycv/SearchResultAdapter.java
@@ -0,0 +1,67 @@
+package it.inefficienza.mycv;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * @author Christian Sarnataro
+ */
+public class SearchResultAdapter extends ArrayAdapter<SearchResult> {
+
+    Context context;
+    List<SearchResult> searchResults = null;
+    int layoutResourceId;
+
+    public SearchResultAdapter(Context context, int layoutResourceId, List<SearchResult> searchResults) {
+        super(context, layoutResourceId, searchResults);
+        this.context = context;
+        this.layoutResourceId = layoutResourceId;
+        this.searchResults = searchResults;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+        SearchResultHolder holder = null;
+
+        if(row == null)
+        {
+            LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+            row = inflater.inflate(layoutResourceId, parent, false);
+
+            holder = new SearchResultHolder();
+            holder.matchingTextView = (TextView)row.findViewById(R.id.matching_text);
+            holder.gotoArrow = (ImageView) row.findViewById(R.id.goto_arrow);
+            row.setTag(holder);
+        }
+        else
+        {
+            holder = (SearchResultHolder)row.getTag();
+        }
+
+        SearchResult searchResult = searchResults.get(position);
+        holder.matchingTextView.setText(searchResult.getMatchingText());
+
+        if (searchResult.getPage() == -1) {
+            holder.gotoArrow.setVisibility(View.INVISIBLE);
+        } else {
+            holder.gotoArrow.setVisibility(View.VISIBLE);
+        }
+
+        return row;
+    }
+
+    static class SearchResultHolder
+    {
+        TextView matchingTextView;
+        ImageView gotoArrow;
+    }
+}
